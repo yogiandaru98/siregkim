57fd2714b94d35583a7820d42e3b0bf852975a3d
YOGI ANDARU <83377619+yogiandaru98@users.noreply.github.com>
2023-01-14 13:25:15 +0700

    susun view

---
+++ app/Views/pages/global/dashboard.php
@@ -0,0 +1,29 @@
+<?= $this->extend('templates/sidebar') ?>
+<?= $this->section('content') ?>
+
+<div class="page-heading">
+    <h3><?= $title ?></h3>
+</div>
+<div class="page-content">
+    <section class="row">
+        <div class="col-12 col-lg-9">
+            <div class="row">
+                <!-- <div class="col-6 col-lg-3 col-md-6">
+                </div> -->
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Chart Alumni</h4>
+                    </div>
+                    <div class="card-body">
+                        <div id="chart-profile-visit"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+<?= $this->endSection() ?>
