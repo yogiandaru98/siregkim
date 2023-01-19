9d9ed5443322ff6cd7ba46797aca7e964442ae49
karinadityas <karina.adityasramadhanti2041@students.unila.ac.id>
2023-01-19 14:20:29 +0700

    tiny edit

--- app/Views/pages/auth/login.php
+++ app/Views/pages/auth/login.php
@@ -20,58 +20,58 @@
 
 <body>
     <div id="auth">
-        
-<div class="row h-100">
-    <div class="col-lg-5 col-12">
-        <div id="auth-left">
-            <div class="auth-logo">
-                <a href="dashboard"><img src="assets/images/logo/Unila.png" alt="Logo"></a>
-            </div>
-            <h1 class="auth-title">SIREGKIM</h1>
-            <form action="/login/action" method="POST" >
-            <?php if(session()->getFlashdata('error')): ?>
-            <div class="alert alert-danger alert-dismissible show fade">
-                <div class="alert-body">
-                    <button class="close" data-dismiss="alert">
-                        <span>&times;</span>
-                    </button>
-                    <b>Gagal!</b>
-                    <?= session()->getFlashdata('error') ?>
-                </div>
-            </div>
-            <?php endif;?>
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="text" name="username" class="form-control form-control-xl" placeholder="Username">
-                    <div class="form-control-icon">
-                        <i class="bi bi-person"></i>
-                    </div>
-                </div>
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="password" name="password" class="form-control form-control-xl" placeholder="Password">
-                    <div class="form-control-icon">
-                        <i class="bi bi-shield-lock"></i>
-                    </div>
-                </div>
-                <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
-            </form>
-            <div class="text-center mt-3">
-                <p><a class="font-bold" href="forgotpass">Forgot password?</a></p>
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-7 d-none d-lg-block">
-        <div id="auth-right">
 
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <div class="auth-logo">
+                        <a href="dashboard"><img src="assets/images/logo/Unila.png" alt="Logo"></a>
+                    </div>
+                    <h1 class="auth-title">SIREGKIM</h1>
+                    <form action="/login/action" method="POST">
+                        <?php if(session()->getFlashdata('error')): ?>
+                        <div class="alert alert-danger alert-dismissible show fade">
+                            <div class="alert-body">
+                                <button class="close" data-dismiss="alert">
+                                    <span>&times;</span>
+                                </button>
+                                <b>Gagal!</b>
+                                <?= session()->getFlashdata('error') ?>
+                            </div>
+                        </div>
+                        <?php endif;?>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="text" name="username" class="form-control form-control-xl"
+                                placeholder="Username">
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" name="password" class="form-control form-control-xl"
+                                placeholder="Password">
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
+
+                </div>
+            </div>
         </div>
-    </div>
-</div>
 
     </div>
 </body>
 <script src="<?= base_url('assets/js/bootstrap.js')?> "></script>
-    <script src="<?= base_url('assets/js/app.js')?> "></script>
+<script src="<?= base_url('assets/js/app.js')?> "></script>
 
-    <!-- Need: Apexcharts -->
-    <script src="<?= base_url('assets/extensions/apexcharts/apexcharts.min.js')?> "></script>
-    <script src="<?= base_url('assets/js/pages/dashboard.js')?> "></script>
+<!-- Need: Apexcharts -->
+<script src="<?= base_url('assets/extensions/apexcharts/apexcharts.min.js')?> "></script>
+<script src="<?= base_url('assets/js/pages/dashboard.js')?> "></script>
+
 </html>
